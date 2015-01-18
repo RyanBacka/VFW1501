bfa9681d09a6f2e5e5cc0074a14dacc68ff8f17c
RyanBacka <RKBacka@Fullsail.edu>
2015-01-17 19:17:21 -0500

    added second window and called to main file

--- Backa_Ryan_Project2/Resources/DescWindow.js
+++ Backa_Ryan_Project2/Resources/DescWindow.js
@@ -1,5 +1,22 @@
-var descWindow = Ti.UI.createWindow({
-	backgroundColor: "fff",
-	backgroundRepeat: true,
-	title: "Definition"
-});
+var definition = function(){
+	var descWindow = Ti.UI.createWindow({
+		backgroundColor: "fff",
+		backgroundRepeat: true,
+		title: "Definition"
+	});
+	var descTitleView = Ti.UI.createView({
+		top: 20,
+		height: 40,
+		backgroundColor:"#bdbdbd"
+	});
+	
+	var descTitleText = Ti.UI.createLabel({
+		text: "Well Testing Equipment Terms and Definitions",
+		font: {fontSize: 14, fontFamily: "Arial"},
+		color: "#fff",
+		center: titleView
+	});
+	
+	descTitleView.add(descTitleText);
+	descWindow.add(descTitleView);
+};
